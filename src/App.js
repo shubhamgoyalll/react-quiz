bb6d287957cdb8f66393fa59db3b134b5ab95754
Shubham Goyal <goyalshubham2521@gmail.com>
2023-07-27 17:13:11 +0530

    Added Server to load JSON data from own API

--- src/App.js
+++ src/App.js
@@ -2,8 +2,36 @@ import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Loader from "./Loader";
 import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  //'loading','error','active','ready','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
