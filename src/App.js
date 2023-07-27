ebb8807cec055e22bcfe3404756ba0628ed1c05c
Shubham Goyal <goyalshubham2521@gmail.com>
2023-07-28 00:59:12 +0530

    Added onClick on Lets Start button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Loader from "./Loader";
 import Main from "./Main";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 import { useEffect, useReducer } from "react";
 
 const initialState = {
@@ -19,6 +20,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action Unknown");
   }
@@ -43,7 +46,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,14 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>WELCOME TO THE REACT QUIZ!</h2>
       <h3>{numQuestions} questions to test your React Mastery</h3>
-      <button className="btn btn-ui">Let's Start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's Start
+      </button>
     </div>
   );
 }
