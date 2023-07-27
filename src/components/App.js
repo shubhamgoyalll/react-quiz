787f49c524bd869dfb7213e712809db773153a60
Shubham Goyal <goyalshubham2521@gmail.com>
2023-07-28 01:23:10 +0530

    Displaying Question and their answer

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
 
   //'loading','error','active','ready','finished'
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -29,7 +30,10 @@ function reducer(state, action) {
 
 export default function App() {
   //Rather than state, we have destructure here only
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -49,7 +53,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
