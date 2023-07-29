b4f0597d5c3f566b26b9c032c5a94299978bbe43
Shubham Goyal <goyalshubham2521@gmail.com>
2023-07-30 00:41:39 +0530

    Added a Restart quiz button

--- src/components/App.js
+++ src/components/App.js
@@ -49,6 +49,12 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
     default:
       throw new Error("Action Unknown");
   }
@@ -108,6 +114,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
